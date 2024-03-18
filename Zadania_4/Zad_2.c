ef47b68d7fb67b47ef182bf662254250eadb2f98
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-03-18 23:23:50 +0100

    Made some changes

--- Zadania_4/Zad_2.c
+++ Zadania_4/Zad_2.c
@@ -1,5 +1,32 @@
 #include <stdio.h>
+#include <stdbool.h>
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
 
+// An optimized version of Bubble Sort
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    bool swapped;
+    for (i = 0; i < n - 1; i++) {
+        swapped = false;
+        for (j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(&arr[j], &arr[j + 1]);
+                swapped = true;
+            }
+        }
+
+        // If no two elements were swapped by inner loop,
+        // then break
+        if (swapped == false)
+            break;
+    }
+}
 int binary_search_recursive(int *arr, int left, int right, int searchValue) {
     if (right >= left) {
         int mid = left + (right - left) / 2;
@@ -16,11 +43,12 @@ int binary_search_recursive(int *arr, int left, int right, int searchValue) {
 
 int main() {
     int arr[] = {45, 6, 14, 68, 99,101,25,103,49};
-    int n = sizeof(arr) / sizeof(arr[0]);
+    int size = sizeof(arr) / sizeof(arr[0]);
     printf("Podaj liczbe ktora chcesz wyszukac: \n");
     int target;
+    bubbleSort(arr,size);
     scanf("%d",&target);
-    int result = binary_search_recursive(arr, 0, n - 1, target);
+    int result = binary_search_recursive(arr, 0, size - 1, target);
 
     if (result == -1) {
         printf("Element nie znajduje sie w tablicy");
