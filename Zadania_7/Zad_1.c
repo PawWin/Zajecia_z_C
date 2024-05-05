8008785a4d4f30a13785c826a538cd9f2d4e4720
Paweł Winkler <147107563+PawWin@users.noreply.github.com>
2024-05-05 17:02:06 +0200

    touched up ex 2

--- Zadania_7/Zad_1.c
+++ Zadania_7/Zad_1.c
@@ -26,25 +26,25 @@ int calculateEnergySum(const char *filename, int option) {
     char* words[] = {"zero","one", "two", "three" ,"four", "five" ,"six" ,"seven", "eight", "nine"};
 
     int sum = 0;
-    char line[80];
+    char line[100];
 
     while (fgets(line, sizeof(line), file)) {
         line[strcspn(line, "\n")] = 0;
 
         int number_front = -1;
         int number_back = -1;
-        int actual_line_len = strlen(line);
+        int line_len = strlen(line);
 
         if (option == 1) {
             // Check from the beginning
-            for (int i = 0; i < actual_line_len; i++) {
+            for (int i = 0; i < line_len; i++) {
                 if (isdigit(line[i])) {
                     number_front = line[i] - '0';
                     break;
                 }
             }
             // Check from the end
-            for (int i = actual_line_len - 1; i >= 0; i--) {
+            for (int i = line_len - 1; i >= 0; i--) {
                 if (isdigit(line[i])) {
                     number_back = line[i] - '0';
                     break;
@@ -52,7 +52,7 @@ int calculateEnergySum(const char *filename, int option) {
             }
         } else if (option == 2) {
             // Check from the beginning and end for words
-            for (int i = 0; i < actual_line_len; i++) {
+            for (int i = 0; i < line_len; i++) {
                 if (isdigit(line[i])) {
                     number_front = line[i] - '0';
                     break;
@@ -68,7 +68,7 @@ int calculateEnergySum(const char *filename, int option) {
                     }
                 }
             }
-            for (int i = actual_line_len - 1; i >= 0; i--) {
+            for (int i = line_len - 1; i >= 0; i--) {
                 if (isdigit(line[i])) {
                     number_back = line[i] - '0';
                     break;
@@ -101,5 +101,7 @@ int main() {
     int energySum2 = calculateEnergySum(filename, 2);
     printf("II Suma wartosci energetycznych: %d\n", energySum2);
 
+    char stayclosed[999];
+    scanf("%s", stayclosed);
     return 0;
 }
--- Zadania_7/Zad_2.c
+++ Zadania_7/Zad_2.c
@@ -3,89 +3,121 @@
 #include <string.h>
 #include <ctype.h>
 
-#define MAX_LINE_LENGTH 180
-#define MAX_COLORS 3
-#define MAX_COLOR_NAME_LENGTH 6  // increased by 1 to accommodate "green"
+#define MAX_CHARS 180
 
-void processFile(const char *filename);
+int extract_game_number(char line[]) {
+    int i = 0;
+    int game_number = 0;
+    int is_game_found = 0;
 
-int main() {
-    const char *filename = "../input2.txt";
-    processFile(filename);
+    // Find the position of "Game"
+    while (line[i] != '\0') {
+        if (line[i] == 'G' && line[i + 1] == 'a' && line[i + 2] == 'm' && line[i + 3] == 'e') {
+            is_game_found = 1;
+            break;
+        }
+        i++;
+    }
 
-    return 0;
+    if (is_game_found) {
+        i += 5; // Skip "Game "
+        while (isdigit(line[i])) {
+            game_number = game_number * 10 + (line[i] - '0');
+            i++;
+        }
+    }
+
+    return game_number;
 }
 
-void processFile(const char *filename) {
+
+int main(){
+    const char *filename = "../input2.txt";
     FILE *file = fopen(filename, "r");
     if (file == NULL) {
-        fprintf(stderr, "Error: Unable to open file %s\n", filename);
+        fprintf(stderr, "Nie mozna otworzyc pliku.\n");
         exit(EXIT_FAILURE);
     }
 
-    int sum = 0;
-    int min_sum = 0;
-    int max_colors[] = {12, 13, 14};
-    char line[MAX_LINE_LENGTH];
+    int sum_id = 0;
+    int moc_sum = 0;
+    char line[MAX_CHARS];
+    int max_red = 12;
+    int max_green = 13;
+    int max_blue = 14;
 
-    int min_colors[MAX_COLORS] = {0}; // Moved outside the loop to accumulate minimum powers
+
 
     while (fgets(line, sizeof(line), file)) {
-        int actual_line_len = strlen(line);
+        int line_len = 0;
         int flag = 0;
+        int min_red = 0;
+        int min_green = 0;
+        int min_blue = 0;
 
-        int game_number;
-        sscanf(line + 5, "%d", &game_number);
+        //dlugosc linii
+        for (int i = 0; i < strlen(line); i++){
+            if(line[i] != NULL){
+                line_len++;
+            }
+        }
 
-        for (int i = 8; i < actual_line_len; i++) {
-            int number;
+        int game_number = extract_game_number(line);
+        int number;
+
+        for (int i = 8; i < line_len; i++) {
             if (isdigit(line[i])) {
-                sscanf(line + i, "%d", &number);
-                while (isdigit(line[i])) i++;
-            }
-
-            char color[MAX_COLOR_NAME_LENGTH];
-            sscanf(line + i, "%s", color);
-
-            if (strncmp(color, "red", 3) == 0) {
-                if (number > max_colors[0]) {
-                    flag = 1;
-                }
-                if (number > min_colors[0]) {
-                    min_colors[0] = number;
-                }
-            } else if (strncmp(color, "green", 5) == 0) {
-                if (number > max_colors[1]) {
-                    flag = 1;
-                }
-                if (number > min_colors[1]) {
-                    min_colors[1] = number;
-                }
-            } else if (strncmp(color, "blue", 4) == 0) {
-                if (number > max_colors[2]) {
-                    flag = 1;
-                }
-                if (number > min_colors[2]) {
-                    min_colors[2] = number;
+                if (isdigit(line[i + 1])) {
+                    number = (line[i] - '0') * 10 + (line[i + 1] - '0');
+                    i++;
+                } else {
+                    number = line[i] - '0';
                 }
             }
+
+            if (strncmp(&line[i], "red", 3) == 0) {
+                if (number > max_red) {
+                    flag = 1;
+                }
+                if (number > min_red){
+                    min_red = number;
+                }
+            }
+
+            if (strncmp(&line[i], "green", 5) == 0) {
+                if (number > max_green) {
+                    flag = 1;
+                }
+                if (number > min_green){
+                    min_green = number;
+                }
+            }
+
+            if (strncmp(&line[i], "blue", 4) == 0) {
+                if (number > max_blue) {
+                    flag = 1;
+                }
+                if (number > min_blue){
+                    min_blue = number;
+                }
+            }
+
         }
 
-        printf("Minimum powers: red=%d, green=%d, blue=%d\n", min_colors[0], min_colors[1], min_colors[2]);
+        moc_sum += (min_red * min_green * min_blue);
 
-        int min_product = 1;
-        for (int i = 0; i < MAX_COLORS; i++) {
-            min_product *= min_colors[i];
-        }
-        min_sum += min_product;
-
-        if (!flag) {
-            sum += game_number;
+        if (flag == 0){
+            sum_id += game_number;
         }
     }
 
     fclose(file);
 
-    printf("Sum of game numbers that can be played: %d\n", sum);
-    printf("Sum of powers required to play the games: %d\n", min_sum);
+    printf("Suma numerow gier, ktore mozna rozegrac: %d\n",sum_id);
+    printf("Suma mocy zestawow wymaganych do rozegrania gier: %d",moc_sum);
+
+
+    char stayclosed[999];
+    scanf("%s", stayclosed);
+    return 0;
 }
